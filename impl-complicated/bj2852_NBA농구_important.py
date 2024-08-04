cc716ad8fb9ea056569273bdea51a13225f9d3ea
extinctmule <extinctmule@gmail.com>
2024-08-05 02:06:47 +0900

    Update bj2853 code

--- impl-complicated/bj2852_NBA농구_important.py
+++ impl-complicated/bj2852_NBA농구_important.py
@@ -13,34 +13,21 @@ team1, team2 = 0, 0
 dct = {'1':-1, '2':1}
 
 stamps = []#점수, 시간
-#stamps = [0,0]#초기
 
 for i in range(goals):
     team, clock = input().split()
-    s = int(clock[0]+clock[1])*60+int(clock[3]+clock[4])
-    if stamps:
-        stamps.append([dct[team]+stamps[-1][0], s])
-    else:
-        stamps.append([dct[team], s])
+    m,s = map(int, clock.split(':'))
     
+    stamps.append([dct[team], m*60+s])
+    if i>0:
+        stamps[i][0]+=stamps[i-1][0]
 
 stamps.append([stamps[-1][0],48*60])#경기종료시점
 
 for i in range(1,goals+1):
     if stamps[i-1][0]<0:
         team1+= stamps[i][1]-stamps[i-1][1]
-
     elif stamps[i-1][0]>0:
         team2+= stamps[i][1]-stamps[i-1][1]
 
-def int_to_str(num):
-    m = str(num//60)
-    s = str(num%60)
-    if len(m)==1:
-        m = '0'+m
-    if len(s)==1:
-        s='0'+s
-    return m+':'+s
-
-print(int_to_str(team1))
-print(int_to_str(team2))
+print(f'{team1//60:02d}:{team1%60:02d}\n{team2//60:02d}:{team2%60:02d}')
