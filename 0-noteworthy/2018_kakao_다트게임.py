9a87235140d5bb3a7a8e7ec675d5fef7b3dcdae6
extinctmule <extinctmule@gmail.com>
2024-08-09 13:08:33 +0900

    Update 치킨배달 code

--- 0-noteworthy/2018_kakao_다트게임.py
+++ 0-noteworthy/2018_kakao_다트게임.py
@@ -14,7 +14,7 @@ def solution(dr):
     options={'*':2,'#':-1}
     # 입력값 나누는것부터 시작해보자.
     cur =0
-    for n in range(3):
+    for n in range(3): # 여기의 scores[n][0]<- 0,1,2하드코딩해야하는데 i로 놓는 실수함. 인덱스변수 너무 정신없어서 개선해야함
         i=0
         if dr[cur+i]=='1' and dr[cur+i+1]=='0':
             scores[n][0]=10
@@ -53,3 +53,4 @@ def solution(dr):
 1D2S3T*
 print(solution('1S#2T*3T*'))
 '''
+
