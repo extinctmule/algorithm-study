3c596abac09e7d66969824445b07f7f900869c06
extinctmule <extinctmule@gmail.com>
2024-08-13 02:39:16 +0900

    Update code

--- graph-bfs-dfs-etc/bj1189_컴백홈.py
+++ graph-bfs-dfs-etc/bj1189_컴백홈.py
@@ -15,24 +15,19 @@ N, M, K = map(int, input().split())
 grid = [list(input()) for _ in range(N)]
 
 
-ans = 0
-
-
-def dfs(x, y, count):
-    global ans
-    if count == K and x == 0 and y == M - 1:
-        ans += 1
-    else:
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            nx = x + dx
-            ny = y + dy
-
-            if 0 <= nx < N and 0 <= ny < M and grid[nx][ny] == ".":
-                grid[nx][ny] = "T"
-                dfs(nx, ny, count + 1)
-                grid[nx][ny] = "."
+def dfs(x, y, cnt):
+    if cnt == K and x == 0 and y == M - 1:
+        return 1
+    ans = 0
+    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < N and 0 <= ny < M and grid[nx][ny] == ".":
+            grid[nx][ny] = "T"
+            ans += dfs(nx, ny, cnt + 1)
+            grid[nx][ny] = "."
+    return ans
 
 
 grid[N - 1][0] = "T"
-dfs(N - 1, 0, 1)
-print(ans)
+print(dfs(N - 1, 0, 1))
