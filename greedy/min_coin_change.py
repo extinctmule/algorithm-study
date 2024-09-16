ac9c52ce6f35da6193e432506439293e6f840592
extinctmule <extinctmule@gmail.com>
2024-09-16 23:10:55 +0900

    Comment 추가

--- greedy/min_coin_change.py
+++ greedy/min_coin_change.py
@@ -5,7 +5,7 @@ dp = [-1] * (sum + 1)
 dp[0] = 0
 
 for i in range(1, sum + 1):
-    cnt = float("inf")
+    cnt = float("inf")# 또는 sys.maxsize
     for coin in arr:
         if i - coin >= 0 and dp[i - coin] != -1:
             cnt = min(cnt, dp[i - coin] + 1)
