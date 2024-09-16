50ede90b5e655bb93dd0582f47a8297fc49c7964
extinctmule <extinctmule@gmail.com>
2024-09-16 23:12:50 +0900

    Add code

---
+++ bruteforce/bj1251_단어나누기.py
@@ -0,0 +1,23 @@
+"""
+앞부분: 일단 길이-2까지중에 제일 작은알파벳에서 끊기(같을경우 더 앞)
+중간: 거기서부터 길이-1까지중에 제일 작은거
+뒷부분:ㅇㅇ
+"""
+
+s = input()
+slen = len(s)
+ans = []
+
+for i in range(1, len(s) - 1):
+    for j in range(i + 1, len(s)):
+        first = s[:i]
+        second = s[i:j]
+        third = s[j:]
+
+        first.reverse()
+        second.reverse()
+        third.reverse()
+
+        ans.append("".join(first + second + third))
+
+print(min(ans))
